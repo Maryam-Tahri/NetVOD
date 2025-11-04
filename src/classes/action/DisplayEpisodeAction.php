cd9be8aa0cce1e76fa47ca23cf26dd04b9d03619
MathisWISPELAERE <e66018u@etu.univ-lorraine.fr>
2025-11-04 17:45:52 +0100

    [NewClass] : DisplayEpisodeAction.php

---
+++ src/classes/action/DisplayEpisodeAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace iutnc\netVOD\action;
+
+class DisplayEpisodeAction extends Action
+{
+    public function execute():String {
+        if(!isset($_SESSION["user"])) {
+            return <<<HTML
+<div>Merci de vous connecter pour avoir accès à toutes les fonctionnalités !</div>
+HTML;
+        }
+        
+}
