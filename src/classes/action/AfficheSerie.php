cac34bd415ff50bdd782f80d1d788abf63694930
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2025-11-05 09:59:08 +0100

    [Fix] : askip j'ai changer des truc alors que j'ai juste copier une methode

---
+++ src/classes/action/AfficheSerie.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace iutnc\netVOD\action;
+
+use iutnc\netVOD\render\SerieRenderer;
+use iutnc\netVOD\repository\NetVODRepo;
+
+class AfficheSerie extends Action
+{
+    public function execute(): string
+    {
+        $repo = NetVODRepo::getInstance();
+        $series = $repo->getAllSeries();
+
+        $html = '<div class="catalogue-container">';
+        $html .= '<h1>Catalogue des séries</h1>';
+
+        if (empty($series)) {
+            $html .= '<p class="no-content">Aucune série disponible pour le moment.</p>';
+        } else {
+            $html .= '<div class="series-grid">';
+            foreach ($series as $serie) {
+                $renderer = new SerieRenderer();
+                $html .= $renderer->render($serie);
+            }
+            $html .= '</div>';
+        }
+
+        $html .= '</div>';
+
+        return $html;
+    }
+}
