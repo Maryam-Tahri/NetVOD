a24f095c948b6988005b52932d1e99c133cea123
FissonRonan <ronan.fisson5@etu.univ-lorraine.fr>
2025-11-06 11:15:41 +0100

    [InProgress] :affichage des listes

--- src/classes/render/ListRender.php
+++ src/classes/render/ListRender.php
@@ -1,1 +1,26 @@
 <?php
+
+namespace iutnc\netVOD\render;
+
+use iutnc\netVOD\base\Serie;
+use iutnc\netVOD\base\Liste;
+
+class ListRender
+{
+    public function renderListe(Liste $liste): string
+    {
+        $type = htmlspecialchars($liste->type_list);
+        $id_liste = $liste->id_liste;
+
+        $html = <<<HTML
+        <div class="list-card">
+            <a href="?action=liste&id_liste=$id_liste">
+                <h2>$type</h2>
+            </a>
+        </div>
+        HTML;
+
+        return $html;
+    }
+
+}
