aea3ad7198076f88b8aee55620e1bb56c4a001a7
ThibautL-3 <thibaut.louyot5@etu.univ-lorraine.fr>
2025-11-06 22:02:35 +0100

    [Modif] : On peut désormer renseigné un type d'erreur par un entier (0 par défaut pour pas d'erreur spécifié).

--- src/classes/exception/AuthException.php
+++ src/classes/exception/AuthException.php
@@ -5,7 +5,18 @@ namespace iutnc\netVOD\exception;
 use Exception;
 
 class AuthException extends Exception{
-    function __construct(String $nom){
+
+    /*
+     * 0 = Pas d'erreur spécifiée
+     * 1 = Email pas activé
+     */
+    private int $typeError;
+    function __construct(String $nom, int $typeError = 0){
         parent::__construct(" $nom");
+        $this->typeError = $typeError;
+    }
+
+    public function getTypeError(){
+        return $this->typeError;
     }
 }
