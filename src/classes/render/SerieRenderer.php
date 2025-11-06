eb1c01270b083f5c81fe17f237e0d0d18d5fc360
MathisWISPELAERE <e66018u@etu.univ-lorraine.fr>
2025-11-06 10:55:24 +0100

    [Patch] : SerieRenderer.php

--- src/classes/render/SerieRenderer.php
+++ src/classes/render/SerieRenderer.php
@@ -10,23 +10,16 @@ class SerieRenderer
 {
     public function render(Serie $serie): string
     {
-        $stmt = NetVODRepo::getInstance()->getPDO()->prepare("SELECT avg(note) FROM commentaire INNER JOIN episode ON episode.id_ep = commentaire.id_ep
-                                                                      INNER JOIN serie ON episode.id_serie = serie.id_serie
-                                                                      WHERE serie.titre_serie = :titre");
         $titre = htmlspecialchars($serie->titre);
         $img = htmlspecialchars($serie->cheminImg);
         $id = $serie->id;
-        $stmt->bindParam(':titre',$titre);
-        $stmt->execute();
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        $note = $row['avg(note)']=="" ? 'pas de note' : $row['avg(note)'];
-
-
 
         $html = <<<HTML
         <div class="serie-card">
-            <img class="serie" src="$img" alt="$titre">
-            <h2>$titre ($note)</h2>
+            <a href="?action=serie&id=$id">
+                <img class="serie" src="$img" alt="$titre">
+                <h2>$titre</h2>
+            </a>
         </div>
         HTML;
 
