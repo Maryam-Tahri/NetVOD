a5cfd48e34d2bd97d095af2e31155e7581a4f063
ThibautL-3 <thibaut.louyot5@etu.univ-lorraine.fr>
2025-11-07 16:18:51 +0100

    [NewClass] : TokenGiver.php donne un token et l'enregistre dans la bdd avec son temps d'expiration

---
+++ src/classes/base/TokenGiver.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace iutnc\netVOD\base;
+
+use Exception;
+use iutnc\netVOD\action\Action;
+use iutnc\netVOD\exception\TokenException;
+use iutnc\netVOD\repository\NetVODRepo;
+
+class TokenGiver {
+    public static function createToken(int $id_user, int $delay = 300) : string {
+        if (gettype($delay) != 'integer') {
+            $delay = 300; // Remise par défaut si ce n'est pas un int : protection
+        }
+        // $token = password_hash(bin2hex(random_bytes(16)), PASSWORD_DEFAULT); // Pas besoin
+        try {
+            $token = bin2hex(random_bytes(16));
+        } catch (Exception $e) {
+            throw new TokenException("TokenGiver Exception : Impossible de créer le token" . $e->getMessage());
+        }
+
+        $expiration = date('Y-m-d H:i:s', time() + $delay);
+
+        try {
+            $pdo = NetVODRepo::getInstance()->getPDO();
+        } catch (Exception $e) {
+            throw new TokenException("TokenGiver Exception: Impossible de créer le token: " . $e->getMessage());
+        }
+        $stmt = $pdo->prepare("INSERT INTO tokens (token, id_user, expiration_token) VALUES (?, ?, ?)");
+        $stmt->execute([$token, $id_user, $expiration]);
+        return $token;
+    }
+}
