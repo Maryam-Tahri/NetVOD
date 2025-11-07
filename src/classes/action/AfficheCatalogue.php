560925cd09c8c31308ea819038b26bf34164e601
Maryam Tahri <e88814u@etu.univ-lorraine.fr>
2025-11-07 10:51:22 +0100

    [Modif] : ajout des select pour le filtre genre et public

--- src/classes/action/AfficheCatalogue.php
+++ src/classes/action/AfficheCatalogue.php
@@ -13,6 +13,8 @@ class AfficheCatalogue extends Action
 
         $searchValue = $_GET['search'] ?? '';
         $sortValue = $_GET['sort'] ?? 'titre_serie';
+        $sortGenre = $_GET['genre'] ?? '';
+        $sortPublic = $_GET['public'] ?? '';
 
         $series = $repo->getAllSeries($searchValue, $sortValue);
 
@@ -23,11 +25,28 @@ class AfficheCatalogue extends Action
 <form method="GET" action="">
     <input type="hidden" name="action" value="catalogue">
     <input type="text" name="search" placeholder="Rechercher..." value="{$searchValue}">
+    
     <select name="sort">
         <option value="titre_serie"  {($sortValue === 'titre_serie' : '') } >Titre</option>
         <option value="date_ajout"   {($sortValue === "date_ajout": '')}>Date d’ajout</option>
         <option value="nb_episodes"  {($sortValue=== 'nb_episodes' : '')}>Nombre d’épisodes</option>
     </select>
+    
+    <select name="genre">
+        <option value = <option value="">-- Tous les genres --</option>
+        <option value="action" {($sortGenre === 'action' : '' )}>Action</option>
+        <option value="comédie" {($sortGenre === 'comédie' : '')}>Comédie</option>
+        <option value="drame" {($sortGenre=== 'drame' : '')}>Drame</option>
+        <option value="science-fiction" {($sortGenre === 'science-fiction' : '')}>Science-fiction</option>
+    </select>
+    
+    <select name="public">
+        <option value="">-- Tout public --</option>
+        <option value="enfant" {($sortPublic === 'enfant' : '')}>Enfant</option>
+        <option value="ado" {($sortPublic === 'ado' : '')}>Adolescent</option>
+        <option value="adulte" {($sortPublic === 'adulte' : '')}>Adulte</option>
+    </select>
+    
     <button type="submit">Rechercher / Trier</button>
 </form>
 HTML;
